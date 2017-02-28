907f2a7c4c56be26bbf58afb2f4234b8494c62e6
jzacharuk <jzacharuk@gmail.com>
2017-02-28 08:52:29 -0800

    Configure logging
    Setup logging to use winston and print to console and file.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Index.js');
+const winston = require('winston');
+
+require('./src/configureLogger')({
+  level: 'info',
+  logPath: 'logs/log.txt',
+});
+
+winston.info('Index.js');
---
+++ src/configureLogger.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const printf = require('printf');
+const winston = require('winston');
+
+module.exports = (({
+  level = 'info',
+  filename = './logs/log.txt',
+  maxSize = 1000000, // 1 Megabyte
+  maxFiles = 100,
+  tailable = true,
+  zippedArchive = true,
+}) => {
+  const createFileTransport = () => {
+    // Create the log directory if it does not already exist
+    // Because Winston is too lazy to do it. Thanks Winston.
+    if (!fs.existsSync(path.dirname(filename))) {
+      fs.mkdirSync(path.dirname(filename));
+    }
+
+    return new winston.transports.File({
+      filename,
+      maxSize,
+      maxFiles,
+      tailable,
+      zippedArchive,
+    });
+  };
+
+  const createConsoleTransport = () => new (winston.transports.Console)({
+    formatter(options) {
+      const time = new Date().toISOString();
+      const lvl = options.level.toUpperCase();
+      const message = options.message ? options.message : '';
+      const meta = options.meta && Object.keys(options.meta).length ? ` ${JSON.stringify(options.meta)}` : '';
+
+        // 7 characters to fit "VERBOSE"
+      return printf('%s %-7s %s %s', time, lvl, message, meta);
+    },
+  });
+
+  winston.configure({
+    level,
+    transports: [
+      createConsoleTransport(),
+      createFileTransport(),
+    ],
+  });
+});
