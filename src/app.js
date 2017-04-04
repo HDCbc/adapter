c69b12bac23ec3176db7506e8076f2db162104ae
Jonathan Zacharuk <jzacharuk@gmail.com>
2017-04-04 13:19:43 -0700

    Restructured app functions

--- src/app.js
+++ src/app.js
@@ -3,63 +3,77 @@ const async = require('async');
 const winston = require('winston');
 
 // Import local modules
+const configureLogger = require('./configureLogger');
 const exporter = require('./exporter/exporter');
 const importer = require('./importer/importer');
 
 module.exports = (() => {
-  const run = ({ exportOptions, importOptions, logLevel }) => {
-    // Configure the logger.
-    // This must be done before loading the other local modules that utilize winston.
-    require('./configureLogger')({
+  /**
+   * Initializes the common components of the application.
+   * This must be called before exporting or importing as
+   * the logger needs to be configured.
+   */
+  const init = (logLevel) => {
+    configureLogger({
       level: logLevel,
       logPath: 'logs/log.txt',
     });
+  };
 
-    function doExport(callback) {
-      if (exportOptions === {}) {
-        return callback(null, 'Skipping Export');
-      }
-
-      return async.series([
-        async.apply(exporter.init, exportOptions),
-        async.apply(exporter.run),
-      ], (err, res) => {
-        // TODO - chain propertly. Note this is async.
-        exporter.cleanup((cErr, cRes) => {
-          winston.info('FINISHED EXPORT', err, res);
-          return callback(err, res);
-        });
-      });
+  /**
+   * Performs the export out of the EMR.
+   */
+  const doExport = (exportOptions, callback) => {
+    if (exportOptions === {}) {
+      return callback(null, 'Skipping Export');
     }
 
-    function doImport(callback) {
-      if (importOptions === {}) {
-        return callback(null, 'Skipping Import');
-      }
-
-      return async.series([
-        async.apply(importer.init, importOptions),
-        async.apply(importer.run),
-      ], (err, res) => {
-        // TODO - chain propertly. Note this is async.
-        importer.cleanup((cErr, cRes) => {
-          winston.info('FINISHED IMPORT', err, res);
-          return callback(err, res);
-        });
-      });
-    }
-
-    async.series({
-      export: doExport,
-      import: doImport,
-    }, (err, res) => {
-      // TODO - remove this
-      console.log(err);
-
-      winston.info('TOTALLY FINISHED', err, res);
+    return async.series([
+      async.apply(exporter.init, exportOptions),
+      async.apply(exporter.run),
+    ], (err, res) => {
+      exporter.cleanup(() =>
+        // Note that the cleanup errors and results are ignored.
+         callback(err, res));
     });
   };
 
+  /**
+   * Performs the import into the Universal Schema.
+   */
+  const doImport = (importOptions, callback) => {
+    if (importOptions === {}) {
+      return callback(null, 'Skipping Import');
+    }
+
+    return async.series([
+      async.apply(importer.init, importOptions),
+      async.apply(importer.run),
+    ], (err, res) => {
+      importer.cleanup(() =>
+        // Note that the cleanup errors and results are ignored.
+        callback(err, res));
+    });
+  };
+
+  /**
+   * Run the application.
+   */
+  const run = ({ exportOptions, importOptions, logLevel }) => {
+    async.series({
+      init: async.asyncify(async.apply(init, logLevel)),
+      export: async.apply(doExport, exportOptions),
+      import: async.apply(doImport, importOptions),
+    }, (err, res) => {
+      if (err) {
+        winston.error('Application Failed', err);
+        return;
+      }
+      winston.info('Application Success', res);
+    });
+  };
+
+  // Reveal the public functions
   return {
     run,
   };
